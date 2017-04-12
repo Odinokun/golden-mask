6065d126fc5bef13516d11059ab1fb8a9abbcee6
odinokun <odinokun@gmail.com>
2017-04-12 10:40:36 +0300

    did menu-section

--- src/js/modules/fullPageScroll.js
+++ src/js/modules/fullPageScroll.js
@@ -3,7 +3,7 @@ module.exports = function() {
     // begin Full Page Scroll
     $(document).ready(function() {
         $('#fullpage').fullpage({
-            sectionsColor: ['#f3f2f8', '#f3f2f8', '#13abeb', '#f3f2f8', '#1BBC9B', '#13abeb'],
+            sectionsColor: ['#f3f2f8', '#f3f2f8', '#13abeb', '#f3f2f8', '#f3f2f8', '#13abeb'],
             navigation: true,
             navigationPosition: 'right',
             navigationTooltips: ['Главная', 'О клубе', 'Развлечения', 'Фотогалерея', 'Меню', 'Контакты']
