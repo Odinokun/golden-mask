945eb1153e6db011257ae03bfc69f0df0c670d00
odinokun <odinokun@gmail.com>
2017-04-27 21:39:27 +0300

    пофиксил бали в мобильном разрешении и привел стили в порядок

--- src/js/modules/fullPageScroll.js
+++ src/js/modules/fullPageScroll.js
@@ -1,32 +1,22 @@
 module.exports = function() {
 
   // begin Full Page Scroll
-  // function windowSize(){
-  //   if (($(window).width() <= '767') || ($(window).height() <= '840')){
-  //     $.fn.fullpage.destroy('all');
-  //     // console.log('small screen');
-  //   } else {
-  //     $('#fullpage').fullpage({
-  //       navigation: true,
-  //       navigationPosition: 'right',
-  //       navigationTooltips: ['Главная', 'О клубе', 'Развлечения', 'Фотогалерея', 'Меню', 'Контакты'],
-  //     });
-  //     // console.log('big screen');
-  //   }
-  // }
-
-  function windowSize(){
-    // if (($(window).width() <= '767') || ($(window).height() <= '840')){
-    if (($(window).width() >= '1111') || ($(window).height() <= '900')){
+  function windowSize() {
+    // убираем кастомный скролл для iPad
+    if (($(window).width() == '768') || ($(window).height() == '1024')) {
+      $.fn.fullpage.destroy('all');
+    }
+    // включаем кастомный скролл для десктопов
+    else if (($(window).width() >= '1111') || ($(window).height() >= '900')) {
       $('#fullpage').fullpage({
         navigation: true,
         navigationPosition: 'right',
         navigationTooltips: ['Главная', 'О клубе', 'Развлечения', 'Фотогалерея', 'Меню', 'Контакты'],
       });
-      // console.log('small screen');
-    } else {
+    }
+    // отключаем кастомный скролл для всего остального
+    else {
       $.fn.fullpage.destroy('all');
-      // console.log('big screen');
     }
   }
 
