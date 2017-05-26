a11e074deea5bcca86cb1b2de1f5cb8636433db6
odinokun <odinokun@gmail.com>
2017-05-26 19:41:09 +0300

    изменил скрипт включающий/отключающий кастомный скролл

--- src/js/modules/fullPageScroll.js
+++ src/js/modules/fullPageScroll.js
@@ -7,7 +7,7 @@ module.exports = function() {
       $.fn.fullpage.destroy('all');
     }
     // включаем кастомный скролл для десктопов
-    else if (($(window).width() >= '1111') || ($(window).height() >= '900')) {
+    else if (($(window).width() >= '1111') && ($(window).height() >= '900')) {
       $('#fullpage').fullpage({
         navigation: true,
         navigationPosition: 'right',
@@ -19,6 +19,26 @@ module.exports = function() {
       $.fn.fullpage.destroy('all');
     }
   }
+
+  // // begin Full Page Scroll
+  // function windowSize() {
+  //   // убираем кастомный скролл для iPad
+  //   if (($(window).width() == '768') || ($(window).height() == '1024')) {
+  //     $.fn.fullpage.destroy('all');
+  //   }
+  //   // включаем кастомный скролл для десктопов
+  //   else if (($(window).width() >= '1111') || ($(window).height() >= '900')) {
+  //     $('#fullpage').fullpage({
+  //       navigation: true,
+  //       navigationPosition: 'right',
+  //       navigationTooltips: ['Главная', 'О клубе', 'Развлечения', 'Фотогалерея', 'Меню', 'Контакты'],
+  //     });
+  //   }
+  //   // отключаем кастомный скролл для всего остального
+  //   else {
+  //     $.fn.fullpage.destroy('all');
+  //   }
+  // }
 
   $(window).on('load resize',windowSize);
   // end   Full Page Scroll
